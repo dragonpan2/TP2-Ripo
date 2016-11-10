c4c8155525dd117e762a45c41c08b16da8550297
EmileDebile <emilien.cp@hotmail.com>
2016-11-10 14:23:32 -0500

    Bd test

--- TP2/src/tp2/TP2.java
+++ TP2/src/tp2/TP2.java
@@ -17,6 +17,7 @@ public class TP2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Initial Build");
+        System.out.println("Test");
     }
     
 }
