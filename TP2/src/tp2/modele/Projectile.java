a360395174d902b57451b5b510fe99e388c0f2b5
dragonpan2 <panbeining@hotmail.com>
2016-11-21 17:07:19 -0500

    New classe

---
+++ TP2/src/tp2/modele/Projectile.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp2.modele;
+
+/**
+ *
+ * @author 1566086
+ */
+public class Projectile {
+
+    int joueur;
+    int vitessX; //
+    int vitessY; //
+    int dommage;
+    int positionX;
+    int positionY;
+    int tempsRestant;
+    
+    public Projectile() {
+    }
+    
+    
+    
+}
